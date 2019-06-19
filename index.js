5511fc98307a077c911375410071e01bb06eb815
Omri Lotan <omrilotan@users.noreply.github.com>
2019-06-19 14:36:12 +0300

    git + ipynb = :(

---
+++ index.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch');
+(async() => {
+	try {
+		const {
+			SOURCE,
+			TARGET,
+			MTR_VALUE,
+			MTR_NAME,
+			STK,
+			UA_STRING,
+			ORIGIN,
+		} = process.env;
+
+		const result = await fetch(SOURCE)
+		const value = await result.text();
+		const response = await fetch(`${TARGET}/${value}`, {
+			headers: {
+				'user-agent': UA_STRING,
+				'Origin': ORIGIN
+				[MTR_NAME]: MTR_VALUE
+			}
+		});
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
+})();
